b3a408a81501fc9a8453c987c7f2c8b741c6f477
Tyler Peterson <tylerpeterson8791@gmail.com>
2023-12-29 23:02:43 -0600

    pseudocode

---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+// Start with two global variables set at zero to Start
+
+// 1. A clicker that goes every time an answer is chosen, this will dictate what gets populated into the page when it flips. 0=Start Quiz page, 1 = q1 and so onabort.apply.apply.apply
+// 2. A timer that starts when the quiz begins and stops when it ends? (does it have to be timed?)
+
+// I need a way to track correct and incorrect.  Then on the final page this tally will be displayed.
+
+// When clicker reaches a number the corresponding text is displayed in the question field and all the answer buttons.A
+
+// Make buttons 2-4 invisible on start page (only have "start quiz" button) and final page (maybe a "try again" button).
+// Declare these html pointers globally at the begining.A
+
+// Acceptance Criteria
+
+// GIVEN I am taking a code quiz
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and score
